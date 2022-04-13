5d6bc9b1180816a9bd656ddee320641e2cc49194
Awu Chen <awu.mxr@gmail.com>
2022-04-12 20:24:57 -0700

    - updated contract and token address to MAINNET

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-        contractAddress : "KT1NeQyyurXAv8pMyL3w57QZZEyKnHVJHwaq", 
-        tokenAddress: "KT19ZS9VhwcAYHCodWaNgws42VVHGTsXSZFU"
+        contractAddress : "KT1EyKfUeWYMbuSC3ceK5YFh3dJVFB3rHVJ1", 
+        tokenAddress: "KT1Bw4BmWFFSHczw7Lb2pT8Vy5Hfk41mYz19"
 }
 
 export default config;
